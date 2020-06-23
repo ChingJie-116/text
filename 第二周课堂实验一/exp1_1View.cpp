0551f47c6c6b0aa29a508fcd182b018df052bdf3
ChingJie-116 <61953417+ChingJie-116@users.noreply.github.com>
2020-06-24 07:47:21 +0800

    Add files via upload
    0302

---
+++ 第二周课堂实验一/exp1_1View.cpp
@@ -0,0 +1,120 @@
+
+// exp1_1View.cpp : Cexp1_1View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "exp1_1.h"
+#endif
+
+#include "exp1_1Doc.h"
+#include "exp1_1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Cexp1_1View
+
+IMPLEMENT_DYNCREATE(Cexp1_1View, CView)
+
+BEGIN_MESSAGE_MAP(Cexp1_1View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+//	ON_WM_LBUTTONDOWN()
+//	ON_WM_LBUTTONUP()
+//ON_WM_LBUTTONUP()
+ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// Cexp1_1View 构造/析构
+
+Cexp1_1View::Cexp1_1View()
+{
+	// TODO: 在此处添加构造代码
+	
+}
+
+Cexp1_1View::~Cexp1_1View()
+{
+}
+
+BOOL Cexp1_1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// Cexp1_1View 绘制
+
+void Cexp1_1View::OnDraw(CDC* pDC)
+{
+	Cexp1_1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// Cexp1_1View 打印
+
+BOOL Cexp1_1View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void Cexp1_1View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void Cexp1_1View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// Cexp1_1View 诊断
+
+#ifdef _DEBUG
+void Cexp1_1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void Cexp1_1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+Cexp1_1Doc* Cexp1_1View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Cexp1_1Doc)));
+	return (Cexp1_1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Cexp1_1View 消息处理程序
+
+
+
+
+void Cexp1_1View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+				CDC *pDC = this->GetDC();
+				int r = rand() % 50+5;
+				pDC->Ellipse(point.x - r, point.y - r, point.x + r, point.y + r);
+	CView::OnLButtonDown(nFlags, point);
+}
